4e8eaa7a423b79751c3aa43f8dd3951ff27c3a4e
Arsenii Hlazov <glazov.arseniy@gmail.com>
2023-10-09 20:19:19 +0300

    news: add str to models

--- news/models.py
+++ news/models.py
@@ -9,15 +9,27 @@ class Article(models.Model):
     tags = models.CharField(max_length=400)
     content = models.TextField()
 
+    def __str__(self):
+        return f"{self.name} - {self.author}"
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
     registration_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name}"
 
 class Comment(models.Model):
     content = models.TextField()
     article = models.ForeignKey("Article", related_name="comments", on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name="comments", on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return f"{self.article} - {self.user}"
 
 class Bookmark(models.Model):
     article = models.ForeignKey("Article", related_name="bookmarks", on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name="bookmarks", on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return f"{self.user}"
