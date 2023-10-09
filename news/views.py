50607fb807b791520d64096fcf281de560d54d74
Arsenii Hlazov <glazov.arseniy@gmail.com>
2023-10-09 19:22:53 +0300

    news: add base template, modify view functions

--- news/views.py
+++ news/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render()
+    return render(request, "base.html")
 
 def login(request):
-    return render()
+    return render(request, "base.html")
 
 # www.newsportal.com/article/5/
 # www.newsportal.com/article/?article_id=5
 def article(request, id):
-    return render()
+    return render(request, "base.html")
 
 def search(request):
-    return render()
+    return render(request, "base.html")
 
 def bookmarks(request):
-    return render()
+    return render(request, "base.html")
 
 def register(request):
-    return render()
+    return render(request, "base.html")
