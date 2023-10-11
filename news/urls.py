c670673f2bb406ddd4cd8aed5b5a644d7fb8851b
Arsenii Hlazov <glazov.arseniy@gmail.com>
2023-10-11 21:14:17 +0300

    news: add views

--- news/urls.py
+++ news/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login"),
+    path("login/", views.login_user, name="login"),
     path("article/<int:id>/", views.article, name="article"),
+    path("article/<int:id>/comment/", views.comment, name="comment"),
     path("search/", views.search, name="search"),
     path("bookmarks/", views.bookmarks, name="bookmarks"),
-    path("register/", views.register, name="register")
+    path("article/<int:id>/bookmark/", views.add_bookmark, name="add_bookmark"),
+    path("register/", views.register, name="register"),
+    path("logout/", views.logout_user, name="logout"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,26 +1,88 @@
-from django.shortcuts import render
-from .models import Article
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import Article, User, Comment, Bookmark
 
 # Create your views here.
 def index(request):
     articles = Article.objects.all()[:10]
     return render(request, "index.html", {"articles": articles})
 
-def login(request):
-    return render(request, "base.html")
+def login_user(request):
+    if request.method == "POST":
+        name = request.POST["user_name"]
+        password = request.POST["password"]
+        user = authenticate(request, username=name, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "login.html", {"authenticated": False})
+    else:
+        return render(request, "login.html")
 
 # www.newsportal.com/article/5/
 # www.newsportal.com/article/?article_id=5
 def article(request, id):
     article = Article.objects.get(id=id)
-    return render(request, "article.html", {"article": article})
+    in_bookmarks = False
+    for bookmark in request.user.bookmarks.all():
+        if article == bookmark.article:
+            in_bookmarks = True
+    return render(request, "article.html", {"article": article, "in_bookmarks": in_bookmarks})
+
+def comment(request, id):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            content = request.POST["content"]
+            article = Article.objects.get(id=id)
+            user = request.user
+            comment = Comment(content=content, article=article, user=user)
+            comment.save()
+            return redirect("article", id)
+        else:
+            return render(request, "comment.html")
+    else:
+        return redirect("article", id)
 
 def search(request):
     return render(request, "base.html")
 
 def bookmarks(request):
-    return render(request, "base.html")
+    if request.user.is_authenticated:
+        return render(request, "bookmarks.html")
+    else:
+        return redirect("index")
+
+def add_bookmark(request, id):
+    article = Article.objects.get(id=id)
+    
+    for bookmark in request.user.bookmarks.all():
+        if article == bookmark.article:
+            bookmark.delete()
+            return redirect("article", id)
+        
+    bookmark = Bookmark(article=article, user=request.user)
+    bookmark.save()
+    return redirect("article", id)
 
 def register(request):
-    
-    return render(request, "register.html")
+    if request.method == "POST":
+        # name = request.POST["first_name"]
+        # lastname = request.POST["last_name"]
+        username = request.POST["user_name"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+        if password != password2:
+            return render(request, "register.html", {"invalid_password": True})
+        else:
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            return redirect("index")
+    else:
+        return render(request, "register.html")
+
+def logout_user(request):
+    logout(request)
+    return redirect("index")
